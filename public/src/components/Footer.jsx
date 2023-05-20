ffde776567ba59eff43beb2cbbd9a14d8f7196a0
idhayadhulla <126640408+idhayadhulla@users.noreply.github.com>
2023-05-20 22:33:13 +0530

    files Added
    react files

---
+++ public/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const year=new Date().getFullYear();
+function Footer(){
+    const year=new Date().getFullYear();
+   return (<footer>
+
+   <p>Copyright © {year}</p>;
+</footer>
+   );
+}
+
+export default Footer;
